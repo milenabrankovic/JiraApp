e83de0d71def8105807e7b744531ebe1b81d6b34
milena <milena>
2019-07-16 16:29:14 +0200

    Vue settings done

---
+++ resources/js/components/NavComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div style="background:red;">
+        <h1>NAVdfkjs</h1>
+    </div>
+</template>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div id="app">
+        <div class="container">
+            @yield('content')
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
